fcda449f6d611f3ba66f6c15d384342bd6002f47
SergioD2202 <sergioalejandrods1609@gmail.com>
2022-05-07 02:40:56 -0400

    main page and basic navbar

--- design-challenge/src/app/app.module.ts
+++ design-challenge/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CardListComponent } from './components/card-list/card-list.component';
+import { CardComponent } from './components/card/card.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    CardListComponent,
+    CardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
