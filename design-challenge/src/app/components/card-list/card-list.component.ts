02bf82334d7f584ecb255bac181fbaa0526c2de8
SergioD2202 <sergioalejandrods1609@gmail.com>
2022-05-13 03:43:02 -0400

    first version of the screen

--- design-challenge/src/app/components/card-list/card-list.component.ts
+++ design-challenge/src/app/components/card-list/card-list.component.ts
@@ -9,9 +9,38 @@ export class CardListComponent implements OnInit {
 
   products:any[] = [true,false,true,false,true,false,false,false,true,true,false,false,true,true,true];
 
+  //Select words
+
+  gameSelect:string = 'Select a game';
+  priceSelect:string = 'All';
+  itemTypeSelect:string = 'All';
+  sortedBySelect:string = 'Featured';
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onChangeFilter(event:any) {
+
+    switch(event.target.id) {
+      case 'games':
+        this.gameSelect = event.target.value;
+        break;
+
+      case 'prices':
+        this.priceSelect = event.target.value;
+        break;
+
+      case 'item-types':
+        this.itemTypeSelect = event.target.value;
+        break;
+
+      case 'sorted-by':
+        this.sortedBySelect = event.target.value;
+        break;
+    }
   }
 
 }
